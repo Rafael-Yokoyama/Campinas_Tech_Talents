2699b549e526d3c6f6b8ba3a1e352e6c5c61a7b3
Rafael <rafaelyokoyama925@gmail.com>
2021-01-21 09:47:25 -0300

    excluido  comentários

--- Trilha_ABInBev/aula9/lista-de-compras/src/Item.js
+++ Trilha_ABInBev/aula9/lista-de-compras/src/Item.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Item = (props) => {
   return (
-    // <li style="color: red;"></li>
+    
     <p style={{color: props.cor}}>
       {props.nome} custa {props.preco}
     </p>
